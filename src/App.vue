5cf4a440a4edbc2fc1ddc5e63358182d3fbbb0fd
Yurk-do <yurk-do@yandex.ru>
2021-08-24 17:36:02 +0300

    make component PaginationComponent

--- src/App.vue
+++ src/App.vue
@@ -1,102 +1,22 @@
 <template>
   <div id="app">
     <h1>List countries</h1>
-    <div v-for="item in resultList" :key="item.id">
-      {{ item.name }} - {{ item.population }}
-    </div>
-    <div class="paginationButtonWrapper">
-      <PaginationButton
-        :buttonName="back"
-        @pagination-button-click="changePage"
-      ></PaginationButton>
-      <PaginationButton
-        :buttonName="this.getButtonPage + 0"
-        :class="[this.getButtonPage + 0 === this.page ? activeClass : '']"
-        @pagination-button-click="changePage"
-      ></PaginationButton>
-      <PaginationButton
-        :buttonName="this.getButtonPage + 1"
-        @pagination-button-click="changePage"
-        :class="[this.getButtonPage + 1 === this.page ? activeClass : '']"
-      ></PaginationButton>
-      <PaginationButton
-        :buttonName="this.getButtonPage + 2"
-        @pagination-button-click="changePage"
-        :class="[this.getButtonPage + 2 === this.page ? activeClass : '']"
-      ></PaginationButton>
-      <PaginationButton
-        :buttonName="forward"
-        @pagination-button-click="changePage"
-      ></PaginationButton>
-    </div>
+    <PaginationComponent :itemsList="this.countryList" :limit="5"></PaginationComponent>
   </div>
 </template>
 
 <script>
 import { countryList } from "./services/countries.js";
-import PaginationButton from "./components/PaginationButton.vue";
+import PaginationComponent from "./components/PaginationComponent.vue";
 export default {
   name: "App",
   components: {
-    PaginationButton,
+    PaginationComponent,
   },
-
   data() {
     return {
-      params: {
-        offset: 0,
-        limit: 5,
-      },
       countryList,
-      page: 1,
-      resultList: "",
-      back: "<",
-      forward: ">",
-      displayData: "",
-      buttonNumber: 0,
-      activeClass: "active",
     };
-  },
-  computed: {
-    getEndPage() {
-      return Math.ceil((this.countryList.length - 1) / this.params.limit);
-    },
-    getButtonPage() {
-      if (this.page === this.getEndPage) {
-        return this.page - 2;
-      }
-      if (this.page === this.getEndPage - 1) {
-        return this.page - 1;
-      } else {
-        return this.page;
-      }
-    },
-  },
-
-  mounted() {
-    this.resultList = this.getUserFromBackEnd(this.params);
-  },
-  methods: {
-    getUserFromBackEnd(params) {
-      return this.countryList.slice(
-        params.offset,
-        params.limit + params.offset
-      );
-    },
-
-    changePage(page) {
-      if (this.page === 1 && page === this.back) return;
-      if (this.page === this.getEndPage && page === this.forward) return;
-      if (page === this.back) {
-        this.page = this.page - 1;
-      } else if (page === this.forward) {
-        this.page = this.page + 1;
-      } else {
-        this.page = page;
-      }
-      this.params.offset = (this.page - 1) * this.params.limit;
-      this.resultList = this.getUserFromBackEnd(this.params);
-    },
   },
 };
 </script>
@@ -110,12 +30,5 @@ export default {
   color: #2c3e50;
   margin-top: 100px;
   font-size: 40px;
-}
-.paginationButtonWrapper {
-  display: flex;
-  justify-content: center;
-}
-.active {
-  background-color: rgb(147, 158, 43);
 }
 </style>
