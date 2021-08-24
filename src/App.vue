593145961f196afa372d108de51be953d9848e8b
Yurk-do <yurk-do@yandex.ru>
2021-08-24 15:35:59 +0300

    homework update pagination

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,104 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>List countries</h1>
+    <div v-for="item in resultList" :key="item.id">
+      {{ item.name }} - {{ item.population }}
+    </div>
+    <div class="paginationButtonWrapper">
+      <PaginationButton
+        :buttonName="back"
+        @pagination-button-click="changePage"
+      ></PaginationButton>
+      <PaginationButton
+        :buttonName="this.getButtonPage + 0"
+        :class="[this.getButtonPage + 0 === this.page ? activeClass : '']"
+        @pagination-button-click="changePage"
+      ></PaginationButton>
+      <PaginationButton
+        :buttonName="this.getButtonPage + 1"
+        @pagination-button-click="changePage"
+        :class="[this.getButtonPage + 1 === this.page ? activeClass : '']"
+      ></PaginationButton>
+      <PaginationButton
+        :buttonName="this.getButtonPage + 2"
+        @pagination-button-click="changePage"
+        :class="[this.getButtonPage + 2 === this.page ? activeClass : '']"
+      ></PaginationButton>
+      <PaginationButton
+        :buttonName="forward"
+        @pagination-button-click="changePage"
+      ></PaginationButton>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { countryList } from "./services/countries.js";
+import PaginationButton from "./components/PaginationButton.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    PaginationButton,
+  },
+
+  data() {
+    return {
+      params: {
+        offset: 0,
+        limit: 5,
+      },
+      countryList,
+      page: 1,
+      resultList: "",
+      back: "<",
+      forward: ">",
+      displayData: "",
+      buttonNumber: 0,
+      activeClass: "active",
+    };
+  },
+  computed: {
+    getEndPage() {
+      return Math.ceil((this.countryList.length - 1) / this.params.limit);
+    },
+    getButtonPage() {
+      if (this.page === this.getEndPage) {
+        return this.page - 2;
+      }
+      if (this.page === this.getEndPage - 1) {
+        return this.page - 1;
+      } else {
+        return this.page;
+      }
+    },
+  },
+
+  mounted() {
+    this.resultList = this.getUserFromBackEnd(this.params);
+  },
+  methods: {
+    getUserFromBackEnd(params) {
+      return this.countryList.slice(
+        params.offset,
+        params.limit + params.offset
+      );
+    },
+
+    changePage(page) {
+      if (this.page === 1 && page === this.back) return;
+      if (this.page === this.getEndPage && page === this.forward) return;
+      if (page === this.back) {
+        this.page = this.page - 1;
+      } else if (page === this.forward) {
+        this.page = this.page + 1;
+      } else {
+        this.page = page;
+      }
+      this.params.offset = (this.page - 1) * this.params.limit;
+      this.resultList = this.getUserFromBackEnd(this.params);
+    },
+  },
+};
 </script>
 
 <style>
@@ -23,6 +108,14 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 100px;
+  font-size: 40px;
+}
+.paginationButtonWrapper {
+  display: flex;
+  justify-content: center;
+}
+.active {
+  background-color: rgb(147, 158, 43);
 }
 </style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,94 @@
+<template>
+  <div id="main-container">
+    <h1 id="title-counter">{{ nameTitle }}</h1>
+    <div id="counter-box">
+      <button class="btn-counter btn-plus" @click="counterPlus">+</button>
+      <div id="display">{{ currentValue }}</div>
+      <button class="btn-counter btn-minus" @click="counterMinus">-</button>
+    </div>
+    <button class="btn-reset" @click="counterReset">Reset</button>
+  </div>
+</template>
+<script>
+export default {
+  name: "Counter",
+  props: {
+    inputData: {
+      type: Object,
+      required: false,
+      default: () => {},
+    },
+  },
+  data() {
+    return {
+      nameTitle: "Counter",
+      startValue: 0,
+      currentValue: 0,
+    };
+  },
+  mounted() {
+    console.log("inputData", this.inputData)
+  },
+  methods: {
+    counterPlus() {
+      this.currentValue += 1;
+    },
+    counterMinus() {
+      this.currentValue -= 1;
+    },
+    counterReset() {
+      this.currentValue = this.startValue;
+    },
+  },
+};
+</script>
+<style scoped>
+#main-container {
+  padding-top: 80px;
+  width: 500px;
+  height: 400px;
+  background-color: rgb(53, 48, 48);
+  color: white;
+  margin: 0 auto;
+  border-radius: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+}
+#title-counter {
+  font-size: 50px;
+}
+#counter-box {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.btn-plus {
+  margin-left: 30px;
+}
+
+.btn-minus {
+  margin-right: 30px;
+}
+
+#display {
+  font-size: 150px;
+}
+
+.btn-counter {
+  display: block;
+  font-size: 60px;
+  text-align: center;
+  padding: 15px;
+  width: 100px;
+  border-radius: 50%;
+}
+.btn-reset {
+  width: 30%;
+  margin: 0 auto;
+  padding: 7px;
+  font-size: 30px;
+  border-radius: 60%;
+}
+</style>
